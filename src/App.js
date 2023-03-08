41cb2816638e992c5ba6969115be0c5916fcfb1a
Josephson Eduardo <josephson.eduardo@gmail.com>
2023-03-08 18:55:55 -0500

    User registration

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Catalog from './Catalog';
 import Register from './Register';
+import React, { useState } from 'react';
 
 function App() {
+  const [showRegistration, setShowRegistration] = useState(false);
+  
+  const toggleShowRegistration = (e) => {
+    let showRegistrationFlag = showRegistration;
+    showRegistrationFlag = !showRegistrationFlag;
+    console.log('toggleShowRegistration :: new showRegistrationFlag', showRegistrationFlag);
+    setShowRegistration(showRegistrationFlag);
+  }
+
   return (
     <div className="App">
       <div className="main">
-        <Register />
+        <Register showRegistration={showRegistration} onCloseClick={toggleShowRegistration} />
+        <div className="register-link">
+          <a href="#" onClick={toggleShowRegistration}>Register</a>
+        </div>
         <Catalog />
       </div>
     </div>
--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,14 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
-const Register = () => {
+const Config = {
+    headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+        'Content-Type': 'application/json',
+    }
+};
+
+const Register = ({showRegistration, onCloseClick}) => {
     // firstname, lastname, customer_email, password, confirm_password
     const [firstname, setFirstname] = useState(null);
     const [lastname, setLastname] = useState(null);
@@ -9,26 +17,58 @@ const Register = () => {
     const [first_password, setFirstPassword] = useState(null);
     const [passwords_match, setPasswordsMatch] = useState(true);
     const [registration_done, setRegistrationDone] = useState(false);
+    const [register_success, setRegisterSuccess] = useState(false);
 
     const passwordChangeHandler = (e) => {
         const confirm_password = e.target.value;
         setPasswordsMatch(first_password == confirm_password);
+        if (first_password == confirm_password) {
+            setPassword(confirm_password);
+        }
     };
 
-    let registrationClasses = 'register-overlay';
-    if (registration_done) {
-        registrationClasses = registrationClasses.concat(' hidden');
+    let registrationClasses = 'register-overlay hidden';
+    if (showRegistration) {
+        registrationClasses = 'register-overlay';
     }
     
     const handleRegistration = (e) => {
         e.preventDefault();
-        console.log('handleRegistration', e.target);
-        setRegistrationDone(true);
+        let registrationUrl = process.env.REACT_APP_BASE_URL + 'api/users';
+        let storeId = process.env.REACT_APP_DEFAULT_STORE_ID;
+        let websiteId = process.env.REACT_APP_DEFAULT_WEBSITE_ID;
+        let payload = {
+            customer: {
+                email: customer_email,
+                firstname,
+                lastname,
+                storeId,
+                websiteId
+            },
+            password
+        };
+        axios.post(registrationUrl, payload, Config).then(response => {
+            const { id, email } = response.data;
+            if (id != undefined && email != undefined) {
+                setRegisterSuccess(true);
+            }
+            // onCloseClick(e);
+        }).catch(error => {
+            console.log('Error with registration', error);
+        }).finally(() => {
+            // Nothing to do, for now...
+        });
     }
     
     return (
         <div className={registrationClasses}>
+            <div className="actions">
+                <button className="btn close-btn" onClick={onCloseClick}>Close</button>
+            </div>
             <div className="register-container">
+                <div className="row">
+                    {register_success && <span>Congratulations! You have successfully registered</span>}
+                </div>
                 <div className="row">
                     <div className="form-input-container">
                         <input type="text" name="firstname" className="form-input" onChange={e => setFirstname(e.target.value)} placeholder="First Name" />
